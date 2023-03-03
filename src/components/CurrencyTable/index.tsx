64eddeb3fb2cc49d91efc098423b16305862ee1e
vadimdk <vadim.dk81@gmail.com>
2023-03-03 18:01:56 +0200

    fix:

--- src/components/CurrencyTable/index.tsx
+++ src/components/CurrencyTable/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Grid, Paper } from "@mui/material";
 import TableCell from "./components/TableCell";
 import { useGetExchangeRateQuery } from "../../services/GetRates";
